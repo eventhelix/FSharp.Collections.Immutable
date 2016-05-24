d53e8d5497527af9bbb5e0c07b9d351fb4378f39
XperiAndri <xperiandri@live.ru>
2016-05-24 16:54:41 +0300

    Updated project to .NET Core RC2

---
+++ Source/FSharp.Collections.Immutable/FSharp.Collections.Immutable/Properties/AssemblyInfo.fs
@@ -0,0 +1,43 @@
+﻿namespace FSharp.Collections.Immutable
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharp.Collections.Immutable")>]
+[<assembly: AssemblyDescription("F# bindings for System.Collections.Immutable")>]
+[<assembly: AssemblyCompany("XperiAndri")>]
+[<assembly: AssemblyProduct("FSharp.Collections.Immutable")>]
+[<assembly: AssemblyCopyright("Copyright © XperiAndri 2016")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+#if PORTABLE259
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[<assembly: Guid("0E8D408D-0FBD-47CE-9C54-31007D0019B9")>]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
