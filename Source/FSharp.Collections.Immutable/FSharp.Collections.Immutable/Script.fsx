79cebb2d538f16fbfe928913e040d3cc96859b1c
Andrii Chebukin <xperiandri@live.ru>
2019-07-16 13:37:11 +0300

    Upgraded to .NET Standard 2.0

--- Source/FSharp.Collections.Immutable/FSharp.Collections.Immutable/Script.fsx
+++
@@ -1,3 +0,0 @@
-﻿#r "../packages/System.Collections.Immutable.1.1.36/lib/portable-net45+win8+wp8+wpa81/System.Collections.Immutable.dll"
-#load "immutable-collection-util.fs"
-#load "immutable-list.fs"
